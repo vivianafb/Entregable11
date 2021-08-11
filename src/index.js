eb652bed1a20e8af5192a15dbb15ad969ed4c885
Viviana <vivianafb15@gmail.com>
2021-08-11 16:30:44 -0400

    Implementacion de PUG

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import routerProductos from './routes/productos.js'
+import handlebars from 'express-handlebars'
+import path from 'path';
+
+const app = express();
+const puerto = 8080;
+const server = app.listen(puerto, () =>
+  console.log('Server up en puerto', puerto)
+);
+server.on('error', (err) => {
+  console.log('ERROR ATAJADO', err);
+});
+
+const publicFolderPath = path.resolve(__dirname, '../public');
+app.use(express.static(publicFolderPath));
+app.set('view engine', 'pug');
+const viewsPath = path.resolve(__dirname, '../views');
+app.set('views',viewsPath);
+
+
+// const layoutFolderPath = path.resolve(__dirname, '../views/layouts');
+// const defaultLayerPth = path.resolve(__dirname, '../views/layouts/index.hbs');
+// const partialFolderPath = path.resolve(__dirname, '../views/partial');
+
+// app.engine(
+//   'hbs',
+//   handlebars({
+//     layoutsDir: layoutFolderPath,
+//     partialsDir: partialFolderPath,
+//     defaultLayout: defaultLayerPth,
+//     extname: 'hbs',
+//   })
+// );
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/productos', routerProductos);
